e019705d653d0a355edbc227af340e4c89c1be00
RachelNyakio <krmumo010@gmail.com>
2024-11-07 23:19:17 +0300

    Add files via upload

---
+++ Cat2__Q2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    
+    int scores[2][2] = {{85, 90}, {78, 92}};
+    
+    
+    for (int i = 0; i < 2; i++) {          
+        for (int j = 0; j < 2; j++) {      
+            printf("%d ", scores[i][j]);  
+        }
+        printf("\n");                      
+    }
+
+    return 0;
+    }
